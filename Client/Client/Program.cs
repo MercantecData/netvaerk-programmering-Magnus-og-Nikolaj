83bafd77fae81baf310d4f26c5c6b50531b8b4a5
Magnus <Magnus@p13.dk>
2019-12-09 20:51:41 +0100

    Client Menu

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using EasyConsole;
 
 namespace Client
 {
@@ -10,26 +11,55 @@ namespace Client
         static void Main(string[] args)
         {
 
-            // Ip & Port = Connection
-            IPAddress ip = IPAddress.Parse("127.0.0.1");
-            int port = 18100;
-            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            void Connect()
+            {
+                Console.Clear();
+                string input = Input.ReadString("Please enter an IP Address:");
+                Output.WriteLine("Connecting to: {0}", input);
 
+                try
+                {
+                    // Ip & Port = Connection
+                    IPAddress ip = IPAddress.Parse(input);
+                    int port = 18100;
+                    IPEndPoint endPoint = new IPEndPoint(ip, port);
 
-            // When Connected get a tcp stream running
-            TcpClient client = new TcpClient();
-            client.Connect(endPoint);
-            NetworkStream stream = client.GetStream();
+                    // When Connected get a tcp stream running
+                    TcpClient client = new TcpClient();
+                    client.Connect(endPoint);
+                    NetworkStream stream = client.GetStream();
 
+                    // Sending your username to the server
+                    // string username = Console.ReadLine();
+                    string username = Input.ReadString("Username:");
+                    byte[] buffer = Encoding.UTF8.GetBytes(username);
+                    stream.Write(buffer, 0, buffer.Length);
 
-            // Sending your username to the server
-            string username = Console.ReadLine();
-            byte[] buffer = Encoding.UTF8.GetBytes(username);
-            stream.Write(buffer, 0, buffer.Length);
+                    // Closing the tcp stream
+                    client.Close();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Failed to connect to the server");
+                    Console.ReadLine();
+                }
 
+                Menu();
 
-            // Closing the tcp stream
-            client.Close();
+            }
+
+            void Menu()
+            {
+                Console.Clear();
+                var menu = new EasyConsole.Menu();
+                menu.Add("Connect", () => Connect());
+                menu.Add("Online Users", () => Console.WriteLine("Test"));
+                menu.Display();
+            }
+
+            Menu();
+
         }
+
     }
 }
