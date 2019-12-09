ebad0e2441a891c077d216334828e534cff331e0
Magnus <Magnus@p13.dk>
2019-12-09 18:19:02 +0100

    Client & Server

---
+++ Client/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Ip & Port = Connection
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 18100;
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+
+            // When Connected get a tcp stream running
+            TcpClient client = new TcpClient();
+            client.Connect(endPoint);
+            NetworkStream stream = client.GetStream();
+
+
+            // Sending your username to the server
+            string username = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(username);
+            stream.Write(buffer, 0, buffer.Length);
+
+
+            // Closing the tcp stream
+            client.Close();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ip & Port = Connection
+            int port = 18100;
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint localEndPoint = new IPEndPoint(ip, port);
+
+            // Listening on port 18100
+            TcpListener listener = new TcpListener(localEndPoint);
+            listener.Start();
+
+            // Title
+            Console.WriteLine("Online Users");
+
+            while (true)
+            { 
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[256];
+                int read = -1;
+
+                // While loop that cycles thru every byte
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, read);
+                    Console.WriteLine(message);
+                }
+            }
+
+
+        }
+    }
+}
