ddf9d1713bd67ba4c119138d755b7e1606d01af7
Magnus <Magnus@p13.dk>
2019-12-09 13:00:13 +0100

    Socket

--- Projekt/Projekt/Program.cs
+++ Projekt/Projekt/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Projekt
 {
@@ -7,6 +9,13 @@ namespace Projekt
     {
         static void Main(string[] args)
         {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 13556;
+
+
+
+
+
             Console.WriteLine("Hello World!");
             var text = Console.ReadLine();
             text = "æøå";
