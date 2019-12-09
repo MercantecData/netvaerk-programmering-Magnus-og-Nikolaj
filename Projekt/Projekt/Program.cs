3eab1133152a095bf1410e661e4c84fcc7aa3adb
Magnus <Magnus@p13.dk>
2019-12-09 14:54:25 +0100

    Client

--- Projekt/Projekt/Program.cs
+++ Projekt/Projekt/Program.cs
@@ -10,17 +10,29 @@ namespace Projekt
         static void Main(string[] args)
         {
             IPAddress ip = IPAddress.Parse("127.0.0.1");
-            int port = 13556;
+            int port = 18100;
+            TcpClient client = new TcpClient();
+
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+
+            string text = "Test";
+
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(buffer, 0, buffer.Length);
+
+            client.Close();
 
 
-
-
-
-            Console.WriteLine("Hello World!");
-            var text = Console.ReadLine();
-            text = "æøå";
-            Console.WriteLine(Encoding.ASCII.GetBytes(text));
-            Console.WriteLine(Encoding.UTF8.GetBytes(text));
+            // Console.WriteLine("Hello World!");
+            // var text = Console.ReadLine();
+            // text = "æøå";
+            // Console.WriteLine(Encoding.ASCII.GetBytes(text));
+            // Console.WriteLine(Encoding.UTF8.GetBytes(text));
 
         }
     }
