e7ab6d4abfe6f7ca95317237c1219abf347f3815
Nikolaj Hoggins Jacobsen <nikolajhjacobsen@gmail.com>
2019-12-10 13:53:31 +0100

    meh

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -10,7 +10,30 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            //Client boy
+            TcpClient client = new TcpClient();
+
+            int port = 22010;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            client.Connect(ip, port);
+
+            NetworkStream stream = client.GetStream();
+            GetMessage(stream);
+
+            Console.WriteLine("Write message");
+            string text = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            stream.Write(buffer, 0, buffer.Length);
+
+            client.Close();
+            static async void GetMessage(NetworkStream stream)
+            {
+                byte[] buffer = new byte[256];
+
+                int numOfBytes = await stream.ReadAsync(buffer, 0, 256);
+                string message = Encoding.UTF8.GetString(buffer, 0, numOfBytes);
+
+                Console.WriteLine(message);
+            }
         }
     }
 }
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -10,7 +10,37 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            //Server boy
+            int port = 22010;
+            IPAddress ip = IPAddress.Any;
+            
+            TcpListener listener = new TcpListener(ip, port);
+
+            listener.Start();
+
+            Console.WriteLine("Server started");
+            TcpClient client = listener.AcceptTcpClient();
+
+            NetworkStream stream = client.GetStream();
+            GetMessage(stream);
+
+            Console.WriteLine("Write message:");
+            string userText = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(userText);
+
+            stream.Write(buffer, 0, buffer.Length);
+
+            Console.ReadKey();
+
+            client.Close();
+            static async void GetMessage(NetworkStream stream)
+            {
+                byte[] buffer = new byte[256];
+
+                int numOfBytes = await stream.ReadAsync(buffer, 0, 256);
+                string message = Encoding.UTF8.GetString(buffer, 0, numOfBytes);
+
+                Console.WriteLine(message);
+            }
         }
     }
 }
