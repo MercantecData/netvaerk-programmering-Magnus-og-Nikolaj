602d54dc89887e8d3f750e556c86b7546ef9d1cd
Nikolaj Hoggins Jacobsen <nikolajhjacobsen@gmail.com>
2019-12-10 13:11:57 +0100

    Project reset / New branch

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -10,49 +10,7 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            // Ip & Port = Connection
-            int port = 18100;
-            IPAddress ip = IPAddress.Any;
-            // IPEndPoint localEndPoint = new IPEndPoint(ip, port);
-
-            // Listening on port 18100
-            TcpListener listener = new TcpListener(ip, port);
-            listener.Start();
-
-            // Title
-            Console.WriteLine("Online Users");
-
-            // List of Users
-            List<string> Users = new List<string>();
-
-            while (true)
-            {
-                // Listening on port 18100 and if there is any variation then it will detect it.
-                byte[] buffer = new byte[256];
-                int read = -1;
-
-                // While loop that cycles thru every byte
-                while ((read = listener.AcceptTcpClient().GetStream().Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    string message = Encoding.UTF8.GetString(buffer, 0, read);
-                    if (message == "command:onlineusers")
-                    {
-                        Console.WriteLine("Sending all users");
-                        byte[] bufferResponse = Encoding.UTF8.GetBytes("Response");
-                        listener.AcceptTcpClient().GetStream().Write(bufferResponse, 0, bufferResponse.Length);
-
-                    } else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Online Users");
-                        Users.Add(message);
-                        foreach (var item in Users)
-                        {
-                            Console.WriteLine(item);
-                        }
-                    }
-                }
-            }
+            //Server boy
         }
     }
 }
